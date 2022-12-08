3cf1ba947754a76ddde2f8d5abcab409589bef4d
Arnaud Balland <balland.arnaud@gmail.com>
2022-12-08 17:05:28 +0100

    Modify Modal component + Modal stories

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,3 +1,40 @@
-export const Modal = () => {
-  return (<>Hello World!</>)
+import './modal.css'
+
+export const Modal = ({ className = '', isOpen, setIsModalOpen, children }) => {
+  if (!isOpen) return null
+
+  const closeModal = (e) => {
+    e.preventDefault()
+    setIsModalOpen(false)
+  }
+
+  return (
+    <div
+      className={`${className} reactModalLib`}
+      tabIndex={-1}
+      role="dialog"
+      onClick={(e) => closeModal(e)}
+    >
+      <div
+        className="reactModalLib__container"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          className="reactModalLib__btnClose"
+          onClick={(e) => closeModal(e)}
+        >
+          <svg
+            width="42"
+            height="42"
+            viewBox="0 0 42 42"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M42 4.23L37.77 0L21 16.77L4.23 0L0 4.23L16.77 21L0 37.77L4.23 42L21 25.23L37.77 42L42 37.77L25.23 21L42 4.23Z" />
+          </svg>
+        </button>
+        <div className="reactModalLib__content">{children}</div>
+      </div>
+    </div>
+  )
 }
---
+++ src/stories/Modal.stories.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { storiesOf } from '@storybook/react'
+import { Modal } from '../components/Modal'
+import './styles.css'
+
+const stories = storiesOf('Modal Test', module)
+
+stories.add('Default Modal', () => {
+  const [modalIsOpen, setIsModalOpen] = useState(false)
+  return (
+    <>
+      <button onClick={() => setIsModalOpen(true)}>Open modal</button>
+      <Modal isOpen={modalIsOpen} setIsModalOpen={setIsModalOpen}>
+        <h2>Modal test</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et
+          dignissim nulla. Donec a lorem nec erat varius interdum. Curabitur nec
+          arcu luctus, lacinia magna et, laoreet neque. Nullam fringilla ante ac
+          nibh mattis pretium. Nulla nec dictum urna, nec molestie nisi. Morbi
+          ultrices, neque in dapibus finibus, odio ipsum pellentesque neque,
+          eget elementum dui augue lobortis elit. Sed ut leo laoreet, mollis
+          odio in, hendrerit neque. Sed non odio elit. Vestibulum molestie a
+          ante sit amet posuere. In quis nulla vulputate, fermentum felis nec,
+          lobortis enim. Vestibulum ultricies tempor nunc nec dignissim. Etiam
+          vel tortor auctor, sollicitudin lacus et, imperdiet diam.
+        </p>
+      </Modal>
+    </>
+  )
+})
+
+stories.add('Custom Modal', () => {
+  const [modalIsOpen, setIsModalOpen] = useState(false)
+  return (
+    <>
+      <button onClick={() => setIsModalOpen(true)}>Open modal</button>
+      <Modal
+        className="customModal"
+        isOpen={modalIsOpen}
+        setIsModalOpen={setIsModalOpen}
+      >
+        <div className="customModal__header">
+          <h2>Modal test</h2>
+        </div>
+        <div className="customModal__body">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et
+            dignissim nulla. Donec a lorem nec erat varius interdum. Curabitur
+            nec arcu luctus, lacinia magna et, laoreet neque. Nullam fringilla
+            ante ac nibh mattis pretium. Nulla nec dictum urna, nec molestie
+            nisi. Morbi ultrices, neque in dapibus finibus, odio ipsum
+            pellentesque neque, eget elementum dui augue lobortis elit. Sed ut
+            leo laoreet, mollis odio in, hendrerit neque. Sed non odio elit.
+            Vestibulum molestie a ante sit amet posuere. In quis nulla
+            vulputate, fermentum felis nec, lobortis enim. Vestibulum ultricies
+            tempor nunc nec dignissim. Etiam vel tortor auctor, sollicitudin
+            lacus et, imperdiet diam.
+          </p>
+        </div>
+      </Modal>
+    </>
+  )
+})
